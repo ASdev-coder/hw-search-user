9d7e994f76e500ec24b55db9b9b5732a9f4b40ed
Anton Sklyar <ant.skliar777@gmail.com>
2025-01-14 21:30:40 +0200

    add new generic method find

--- user search hw/Program.cs
+++ user search hw/Program.cs
@@ -5,6 +5,19 @@ using System;
 using System.Collections.Specialized;
 using System.ComponentModel.DataAnnotations;
 
+List<int> ints = new List<int>();
+{
+    ints.Add(1);
+    ints.Add(2);
+    ints.Add(3);
+    ints.Add(4);
+    ints.Add(5);
+    ints.Add(6);
+    ints.Add(7);  
+    ints.Add(8);
+    ints.Add(9);
+    ints.Add(10);
+}
 List<User> users = new List<User>()
 {
     new User (1, "ilya@gmail.com",  15 ),
@@ -19,31 +32,52 @@ List<User> users = new List<User>()
     new User (10, "ilya@gmail.com",  39 ),
     new User (11, "anton@gmail.com",  68 ),
     new User (12, "sasha@gmail.com",  25 ),
-
 };
 
-List<User> Find(List<User> users, Predicate predicate )
+
+
+List<T> Find<T>(List<T> items, Predicate<T> predicate)
 {
+    List<T> result = new List<T>();
 
-    List<User> result = new List<User>();
-
-    foreach ( User user in users )
+    foreach (T item in items)
     {
-        if (predicate(user))
-            result.Add( user );
+        if (predicate(item))
+            result.Add(item);
     }
 
     return result;
-};
+}
 
-// Find<T>()        // <<<<< !!!!!!!    ***
+//List<User> Find(List<User> users, Predicate predicate )
+//{
 
-List<User> foundUsers = Find(users, user => user.Id > 3 && user.Email == "anton@gmail.com" && user.Age > 18);
+//    List<User> result = new List<User>();
 
-foreach( User user in foundUsers )
+//    foreach ( User user in users )
+//    {
+//        if (predicate(user))
+//            result.Add( user );
+//    }
+
+//    return result;
+//};
+
+
+//List<User> foundUsers = Find(users, user => user.Id > 3 && user.Email == "anton@gmail.com" && user.Age > 18);
+
+//foreach (User user in foundUsers)
+//{
+//    Console.WriteLine(user.ToString());
+//}
+
+List<int> foundInts = Find<int>(ints, number => number > 5 && number%2 == 0);
+
+foreach ( int user in foundInts )
 {
     Console.WriteLine(user.ToString());
 }
+
 
 class User
 {
--- user search hw/obj/Debug/net8.0/user search hw.AssemblyInfo.cs
+++ user search hw/obj/Debug/net8.0/user search hw.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("user search hw")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8140c637440dddc646c8e122d366d4213c1e61f")]
 [assembly: System.Reflection.AssemblyProductAttribute("user search hw")]
 [assembly: System.Reflection.AssemblyTitleAttribute("user search hw")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
